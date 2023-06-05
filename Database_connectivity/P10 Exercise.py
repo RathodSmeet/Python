243ebf0fbe199e60ad52c110ca003c4f298ea3c6
RathodSmeet <rathodsmeet2145@gmail.com>
2023-06-05 22:07:12 +0530

    Day-9 work done✨

---
+++ Database_connectivity/P10 Exercise.py
@@ -0,0 +1,9 @@
+"""
+Write a python script to display option as follows,On input specific option,perform appropriate database operation on table with fields of your choice.(Use modulenwhich contains function for database operation)
+1.Display all
+2.Display specific record
+3.insert
+4.Update
+5.Delete
+6.Exit
+"""
