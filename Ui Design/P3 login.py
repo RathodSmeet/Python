6f9279adc490fa2ca17d42361a36f9397996af80
RathodSmeet <rathodsmeet2145@gmail.com>
2023-06-06 23:33:04 +0530

    Day-10 work done✨

---
+++ Ui Design/P3 login.py
@@ -0,0 +1,17 @@
+import tkinter
+win = tkinter.Tk()
+win.geometry("250x250")
+
+lblusername = tkinter.Label(win,text="Username: ")
+lblusername.place(x=10,y=10)
+txtusername = tkinter.Entry(win)
+txtusername.place(x=110,y=50)
+
+lblpassword = tkinter.Label(win,text="Password: ")
+lblpassword.place(x=10,y=70)
+txtpassword = tkinter.Entry(win)
+txtpassword.place(x=110,y=70)
+
+btnlogin = tkinter.Button(win,text="Login")
+btnlogin.place(x=110,y=90)
+win.mainloop()
---
+++ Ui Design/P6 Exercise.py
@@ -0,0 +1,7 @@
+""""Write a python script to create login using tkinter
+
+Connect to database to verify, if username/password is correct.
+
+If, correct, please provide proper message
+
+If not correct, please provide proper error message"""
